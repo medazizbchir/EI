773e4d15e3c9b23374be30eead80e873151c0935
Hamdi GAZZEH <hamdi.gazzeh@student-cs.fr>
2023-06-01 20:16:20 +0200

    nlp

--- dedup_proba.py
+++ dedup_proba.py
@@ -8,11 +8,15 @@ alt.data_transformers.enable('default', max_rows=None)
 import datetime
 
 cancer_condition = {'C50','C500','C501','C502','C503','C504'}
+risk_factors = {'tabac':['Z587','Z720'], 'alcool':['T51','K70','F10'], 'diabete':['E10','E11','E12'], 'sub_psy':['Z864'], 'tum_herit':['Z803']}
 
 df_person = pd.read_pickle('data/df_person.pkl')
 df_visit = pd.read_pickle('data/df_visit.pkl')
 df_condition = pd.read_pickle('data/df_condition.pkl')
 df_dedup_proba = pd.read_pickle('data/df_dedup_proba.pkl')
+
+df_person['gender_source_value'] = df_person['gender_source_value'].replace(['female', 'f'], 'f')
+df_person['gender_source_value'] = df_person['gender_source_value'].replace(['male', 'm'], 'm')
 
 def deduplicate_proba(df_person: pd.DataFrame, df_dedup_proba: pd.DataFrame, score: int):
     #Only keep rows with a probability above the value score
